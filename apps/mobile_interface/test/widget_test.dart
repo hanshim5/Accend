7b3d5a1cc451b608551ac15df4628bd51633e7e9
nicop87 <nicolas.piker@icloud.com>
2026-03-04 13:37:07 -0800

    Changed main.dart to app.dart and updated the test

--- apps/mobile_interface/test/widget_test.dart
+++ apps/mobile_interface/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mobile_interface/main.dart';
+import 'package:mobile_interface/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
